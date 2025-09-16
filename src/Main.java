517db02734a074c9482a5e19eaba6dabfa3a7fa6
GustavoSilvalgs <luisgust06@gmail.com>
2025-09-15 23:37:36 -0300

    Passo 3 + Passo 3.1 + Passo 3.2 + Passo 3.3
    3 – Deve conter uma classe Principal para executar as seguintes ações:
    3.1 – Inserir todos os funcionários, na mesma ordem e informações da tabela acima.
    3.2 – Remover o funcionário “João” da lista.
    3.3 – Imprimir todos os funcionários com todas suas informações, sendo que:
    • informação de data deve ser exibido no formato dd/mm/aaaa;
    • informação de valor numérico deve ser exibida no formatado com separador de milhar como ponto e decimal como vírgula.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import model.Funcionario;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
+        symbols.setDecimalSeparator(',');
+        symbols.setGroupingSeparator('.');
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        funcionarios.add(new Funcionario("Maria", LocalDate.parse("18/10/2000", formatter), new BigDecimal("2009.44"), "Operador"));
+        funcionarios.add(new Funcionario("João", LocalDate.parse("12/05/1990", formatter), new BigDecimal("2284.38"), "Operador"));
+        funcionarios.add(new Funcionario("Caio", LocalDate.parse("02/05/1961", formatter), new BigDecimal("9836.14"), "Coordenador"));
+        funcionarios.add(new Funcionario("Miguel", LocalDate.parse("14/10/1988", formatter), new BigDecimal("19119.88"), "Diretor"));
+        funcionarios.add(new Funcionario("Alice", LocalDate.parse("05/01/1995", formatter), new BigDecimal("2234.68"), "Recepcionista"));
+        funcionarios.add(new Funcionario("Heitor", LocalDate.parse("19/11/1999", formatter), new BigDecimal("1582.72"), "Operador"));
+        funcionarios.add(new Funcionario("Arthur", LocalDate.parse("31/03/1993", formatter), new BigDecimal("4071.84"), "Contador"));
+        funcionarios.add(new Funcionario("Laura", LocalDate.parse("08/07/1994", formatter), new BigDecimal("3017.45"), "Gerente"));
+        funcionarios.add(new Funcionario("Heloísa", LocalDate.parse("24/05/2003", formatter), new BigDecimal("1606.85"), "Eletricista"));
+        funcionarios.add(new Funcionario("Helena", LocalDate.parse("02/09/1996", formatter), new BigDecimal("2799.93"), "Gerente"));
+
+        funcionarios.remove(1);
+
+        System.out.println("Nome | Data Nascimento | Salário | Função");
+        for (Funcionario f : funcionarios) {
+            System.out.println(
+                    f.getNome() +
+                            " | " + f.getDataNascimento().format(formatter) +
+                            " | " + decimalFormat.format(f.getSalario()) +
+                            " | " + f.getFuncao()
+            );
+        }
     }
 }
